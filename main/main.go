4fcf643fae33827fd74d266108b578541ec3abac
Mark Dawson <markdawson@live.com>
2015-08-22 14:12:35 -0700

    Read in from integration json file, generate object model

--- main/main.go
+++ main/main.go
@@ -1,38 +1,41 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/markdaws/gohome"
 	"github.com/markdaws/gohome/www"
 )
 
-func main2() {
-	var x float32 = 123423.452
-	fmt.Printf("hi %.2f\n", x)
-	//	formatter(1, "what", true)
-}
-
-func formatter(args ...interface{}) {
-	for _, val := range args {
-		fmt.Printf("is a something: %v\n", val)
-	}
-}
-
 func main() {
 
 	fmt.Println("creating system")
-	system := createSystem()
+	var sbpID = "1"
+	system, err := importSystem("main/ip.json", sbpID)
+	if err != nil {
+		fmt.Println("Failed to import", err)
+		return
+	}
+
+	//TODO: When to connect, on demand or on start?
+	//TODO: Automatic retry for failed commands?
+	//TODO: Option to listen for events or not, permanent connection
+	//TODO: Log disconnect/reconnect to devicesfor diagnostics purposes
 
 	// TODO: Connection Pool, plus loop through connecting to devices? Only on demand?
-	/*	err := system.Devices["sbp1"].Connection.Connect()
-		if err != nil {
-			panic("Failed to connect to device")
-		} else {
-			fmt.Println("connected")
-		}
-	*/
+	err = system.Devices[sbpID].Connection.Connect()
+	if err != nil {
+		panic("Failed to connect to device")
+	} else {
+		fmt.Println("connected")
+	}
+
 	serverDone := make(chan bool)
 	go func() {
 		s := www.NewServer("./www", system)
@@ -83,13 +86,162 @@ func main() {
 	<-serverDone
 }
 
-func importSystem() *gohome.System {
-	return nil
+func importSystem(integrationReportPath, smartBridgeProID string) (*gohome.System, error) {
+
+	//TODO: dynamic path
+	bytes, err := ioutil.ReadFile(integrationReportPath)
+	if err != nil {
+		return nil, err
+	}
+
+	//TODO: Rename x
+	var x map[string]interface{}
+	if err = json.Unmarshal(bytes, &x); err != nil {
+		return nil, err
+	}
+
+	system := &gohome.System{
+		Identifiable: gohome.Identifiable{
+			Id:          "1",
+			Name:        "Lutron Smart Bridge Pro",
+			Description: "Lutron Smart Bridge Pro - imported //TODO: Date",
+		},
+		Devices: make(map[string]*gohome.Device),
+		Scenes:  make(map[string]*gohome.Scene),
+		Zones:   make(map[string]*gohome.Zone),
+	}
+
+	root, ok := x["LIPIdList"].(map[string]interface{})
+	if !ok {
+		return nil, errors.New("Missing LIPIdList key, or value not a map")
+	}
+	devices, ok := root["Devices"].([]interface{})
+	if !ok {
+		return nil, errors.New("Missing Devices key, or value not a map")
+	}
+
+	fmt.Println("\nDEVICES")
+
+	var makeDevice = func(deviceMap map[string]interface{}) *gohome.Device {
+		var deviceID string = strconv.FormatFloat(deviceMap["ID"].(float64), 'f', 0, 64)
+		var deviceName string = deviceMap["Name"].(string)
+
+		return &gohome.Device{
+			Identifiable: gohome.Identifiable{
+				Id:          deviceID,
+				Name:        deviceName,
+				Description: deviceName},
+			//TODO: Shouldn't set here, comes in from user
+			Connection: &gohome.TelnetConnection{
+				Network:  "tcp",
+				Address:  "192.168.0.10:23",
+				Login:    "lutron",
+				Password: "integration",
+			}}
+	}
+
+	var makeScenes = func(sceneContainer map[string]*gohome.Scene, deviceMap map[string]interface{}, sbp *gohome.Device) error {
+		buttons, ok := deviceMap["Buttons"].([]interface{})
+		if !ok {
+			return errors.New("Missing Buttons key, or value not array")
+		}
+
+		var deviceID string = strconv.FormatFloat(deviceMap["ID"].(float64), 'f', 0, 64)
+		for _, buttonMap := range buttons {
+			button, ok := buttonMap.(map[string]interface{})
+			if !ok {
+				return errors.New("Expected Button elements to be objects")
+			}
+			if name, ok := button["Name"]; ok && !strings.HasPrefix(name.(string), "Button ") {
+				fmt.Printf("  Scene %d: %s\n", int(button["Number"].(float64)), name)
+
+				var buttonID string = strconv.FormatFloat(button["Number"].(float64), 'f', 0, 64)
+				var buttonName = button["Name"].(string)
+				var uniqueID string = deviceID + ":" + buttonID
+				var pressCommand string = "#DEVICE," + deviceID + "," + buttonID + ",3\r\n"
+				var releaseCommand string = "#DEVICE," + deviceID + "," + buttonID + ",4\r\n"
+
+				sceneContainer[uniqueID] = &gohome.Scene{
+					Identifiable: gohome.Identifiable{
+						Id:          uniqueID,
+						Name:        buttonName,
+						Description: buttonName},
+					Commands: []gohome.Command{&gohome.StringCommand{
+						Device: sbp,
+						Value:  pressCommand + releaseCommand,
+					}},
+				}
+			}
+		}
+
+		return nil
+	}
+
+	// First need to find the Smart Bridge Pro since it is needed to make scenes and zones
+	var sbp *gohome.Device
+	for _, deviceMap := range devices {
+		device, ok := deviceMap.(map[string]interface{})
+		if !ok {
+			return nil, errors.New("Expected Devices elements to be objects")
+		}
+		var deviceID string = strconv.FormatFloat(device["ID"].(float64), 'f', 0, 64)
+		if deviceID == smartBridgeProID {
+			sbp = makeDevice(device)
+			makeScenes(system.Scenes, device, sbp)
+			break
+		}
+	}
+
+	if sbp == nil {
+		return nil, errors.New("Did not find Smart Bridge Pro with ID:" + smartBridgeProID)
+	}
+	system.Devices[smartBridgeProID] = sbp
+
+	for _, deviceMap := range devices {
+		device, ok := deviceMap.(map[string]interface{})
+		if !ok {
+			return nil, errors.New("Expected Devices elements to be objects")
+		}
+
+		fmt.Printf("%d: %s\n", int(device["ID"].(float64)), device["Name"])
+
+		// Don't want to re-add the SBP
+		var deviceID string = strconv.FormatFloat(device["ID"].(float64), 'f', 0, 64)
+		if deviceID == smartBridgeProID {
+			continue
+		}
+		gohomeDevice := makeDevice(device)
+		system.Devices[gohomeDevice.Id] = gohomeDevice
+		makeScenes(system.Scenes, device, sbp)
+	}
+
+	zones, ok := root["Zones"].([]interface{})
+	if !ok {
+		return nil, errors.New("Missing Zones key")
+	}
+
+	fmt.Println("\nZONES")
+	for _, zoneMap := range zones {
+		zone := zoneMap.(map[string]interface{})
+		fmt.Printf("%d: %s\n", int(zone["ID"].(float64)), zone["Name"])
+
+		var zoneID string = strconv.FormatFloat(zone["ID"].(float64), 'f', 0, 64)
+		var zoneName string = zone["Name"].(string)
+		system.Zones[zoneID] = &gohome.Zone{
+			Identifiable: gohome.Identifiable{
+				Id:          zoneID,
+				Name:        zoneName,
+				Description: zoneName},
+			SetCommand: &gohome.StringCommand{Device: sbp, Value: "#OUTPUT," + zoneID + ",1,%.2f\r\n"},
+		}
+	}
+
+	return system, nil
 }
 
+/*
 func createSystem() *gohome.System {
 
-	//TODO: Read in from configuration file
 	sbp := &gohome.Device{gohome.Identifiable{
 		Id:   "sbp1",
 		Name: "Lutron Smart Bridge Pro"},
@@ -169,3 +321,4 @@ func createSystem() *gohome.System {
 	// - Get Zone Intensity
 	return system
 }
+*/
