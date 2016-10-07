825cc49bb0c139029f9b1e8de376fc909155e239
Mark Dawson <markdawson@live.com>
2016-10-07 15:07:58 -0700

    Add outlet type grouping in the zonelist UI

--- www/assets/js/components/ZoneList.jsx
+++ www/assets/js/components/ZoneList.jsx
@@ -8,17 +8,27 @@ var ZoneList = React.createClass({
     render: function() {
         var lightZones = [];
         var shadeZones = [];
+        var outletZones = [];
         var otherZones = [];
         this.props.zones.forEach(function(zone) {
-            var cmpZone = <Zone id={zone.id} name={zone.name} type={zone.type} output={zone.output} key={zone.id}/>;
+            var cmpZone = (
+                <Zone
+                    id={zone.id}
+                    name={zone.name}
+                    type={zone.type}
+                    output={zone.output}
+                    key={zone.id}/>
+            );
 
             switch(zone.type) {
-                    //TODO: Put in enum somewhere
                 case 'light':
                     lightZones.push(cmpZone);
                     break;
                 case 'shade':
                     shadeZones.push(cmpZone);
+                    break;
+                case 'outlet':
+                    outletZones.push(cmpZone);
                     break;
                 default:
                     otherZones.push(cmpZone);
@@ -28,12 +38,22 @@ var ZoneList = React.createClass({
 
         return (
             <div className="cmp-ZoneList row">
+                <div className="clearfix">
                 <h2 className={ClassNames({ 'hidden': lightZones.length === 0})}>Lights</h2>
                 {lightZones}
+                </div>
+                <div className="clearfix">
                 <h2 className={ClassNames({ 'hidden': shadeZones.length === 0})}>Shades</h2>
                 {shadeZones}
+                </div>
+                <div className="clearfix">
+                <h2 className={ClassNames({ 'hidden': outletZones.length === 0})}>Outlets</h2>
+                {outletZones}
+                </div>
+                <div className="clearfix">
                 <h2 className={ClassNames({ 'hidden': otherZones.length === 0})}>Other Zones</h2>
                 {otherZones}
+                </div>
             </div>
         );
     }
